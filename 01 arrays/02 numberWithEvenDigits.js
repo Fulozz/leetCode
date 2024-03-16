bafe99c33be64ebc1525fcb5d53e80c23e3a0f07
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-03-16 13:17:29 -0300

    Correção

--- 01 arrays/02 numberWithEvenDigits.js
+++ 01 arrays/02 numberWithEvenDigits.js
@@ -12,20 +12,13 @@
 const nums = [12,345,2,6,7896]
 
 var findNumbers = function(nums) {
-    // const cannot be used, needed to be an variable to ADD in the looping
-    let count = 0
-    for(let i = 0; i < nums.length; i++){
-        // digits
-        let d  = 0;
-        // While the number be differente than 0
-        while( nums[i] != 0){
-            nums[i] = nums[i]/10;
-            d++;
-        } 
-        // If the looping end, and be divisible by 2 and at finish the waste is 0 ADD count
-        if(d % 2 == 0){
-                count++;
-        }        
+    // const não pode ser utilizado por não ter possivel realizar alterações, precisa ser um let
+    let count = 0;
+    // For each value(v) of numbs, turn in string, if the length is divided and at the end finished in 0 sum in the count
+    for(const v of nums){
+           count += String(v).length % 2 ==0
     }
     return count;
 };
+
+// Expected result is 3 even numbers(2,6,12,7896)
