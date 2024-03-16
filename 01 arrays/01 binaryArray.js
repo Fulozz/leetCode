a6036c18fb58b570777fd14712390ee90686e406
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-03-16 10:53:08 -0300

    Comments and description

--- 01 arrays/01 binaryArray.js
+++ 01 arrays/01 binaryArray.js
@@ -2,8 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-
+/**
+ * file: "arrays/01 binaryArray"
+ * description: file responsible for identifying how many Ones in a row the binary array has
+ * data: 16/03/2024
+ * author: Thiago Silva Andrade
+ * complexity: O(n)
+ */
 
 const findMaxConsecutiveOnes = function(nums){
     let number  = 0;
