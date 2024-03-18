4b52b0e3f40c96816d7741b18b5d06f443d48ae2
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-03-18 09:53:07 -0300

    Alterações de nome, e comentarios

---
+++ 02 - 30 Days of JavaScript/easy/05 transformArray.js
@@ -0,0 +1,29 @@
+/**
+ * file: "02 - 30 Days of JavaScript/transformArray"
+ * description: Transform all the inputs in array
+ * data: 18/03/2024
+ * author: Thiago Silva Andrade
+ */
+
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+
+
+// fn can be any function an can be passed in the map to render the results in array
+const fn = function plusone(n) { return n + 1; }
+
+// Maping the datas and transform in array
+var map = function(arr, fn) {
+    // Init the empty array
+    let newArray = []
+    // Use the input array to construct the newArray to setup all the datas inside
+    for(let i = 0; i < arr.length; i++){
+        newArray.push(fn(arr[i], i))
+    }
+    // return the array already with all numbers inside
+    return newArray
+};
