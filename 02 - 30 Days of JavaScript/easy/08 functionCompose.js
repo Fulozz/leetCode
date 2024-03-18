6bb10c916c4f722c6adb07f243124ebd72c10785
Thiago Silva Andrade <92411755+Fulozz@users.noreply.github.com>
2024-03-18 12:16:30 -0300

    functionCompose

--- 02 - 30 Days of JavaScript/easy/08 functionCompose.js
+++ 02 - 30 Days of JavaScript/easy/08 functionCompose.js
@@ -7,25 +7,28 @@
 
 
 /**
- * @param {Function[]} functions
- * @return {Function}
- */
+* @param {Function[]} functions
+* @return {Function}
+*/
 var compose = function(functions) {
-    if(functions.length === 0){
-        return function(x){
-            return x
-        }
-    }
-    return function(x){
-        let result = x
-        for( let i = functions.length; i>=0; i--){
-            result = functions[i](result)
-        }
-        return result
-    }
+   if(functions.length === 0){
+       return function(x){
+           return x
+       }
+   }
+   return function(x){
+    // handle the result of the past function
+       let result = x
+       // use the past result to do next function
+       for( let i = functions.length - 1; i>=0; i--){
+        // Update the result for the next function
+           result = functions[i](result)
+       }
+       return result
+   }
 };
 
 /**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
+* const fn = compose([x => x + 1, x => 2 * x])
+* fn(4) // 9
+*/
